4ca18b83483db1190ab3a8b1523a047cee372a1b
Akshay Asok <akshayasokson@gmail.com>
2025-06-04 17:10:45 +0530

    Requirements updated

--- app/src/LocationSelector.js
+++ app/src/LocationSelector.js
@@ -5,38 +5,119 @@ import { useState, useEffect } from "react";
 function LocationSelector() {
 
     //state variables
-    const [selected, setSelected] = useState({
-      country : ""  ,
-      state : "",
-      city : ""
-    })
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSeletedState] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
 
-    const [locationData, setLocationData] = useState({
-        countries : [],
-        states : [],
-        cities : []
-    })
+
+    const [countryData, setCountryData] = useState([]);
+    const [stateData, setStateData] = useState([]);
+    const [cityData, setCityData] = useState([]);
+
+    
 
 
     // useeffect to fetch the countries data
     useEffect(() => {
         fetch("https://crio-location-selector.onrender.com/countries")
         .then(data => data.json())
-        .then(data => setLocationData({
-            ...locationData,
-            countries : data,
-        }))
+        .then(data => setCountryData(data))
         .catch(error => console.error("Error while fetching : ", error))
     }, [])
+    //  console.log("country >> ", countryData)
 
-    console.log("Countries >> ", locationData)
+    // useeffect to fetch the states data
+    useEffect(() => {
+
+        //if country is updated with valid value, clear selected state and city, and fetch respective states 
+        if(selectedCountry) {
+
+            console.log("selected country >>", selectedCountry);
+
+            fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+            .then(data => data.json())
+            .then((data) => {
+               setStateData(data);
+            })
+            .catch(error => console.error("Error while fetching : ", error))
+            setSeletedState("");
+            setSelectedCity("");
+        }
+
+    }, [selectedCountry])
+    // console.log("state >> ", stateData)
+    
 
 
-    return (<div>
+    // useeffect to fetch the cities data
+    useEffect(() => {
+
+        if (selectedCountry && selectedState){
+            fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
+            .then(data => data.json())
+            .then(data => setCityData(data))
+            .catch(error => {
+                console.error("Error while fetching : ", error);
+                setCityData([]);
+            })
+            setSelectedCity("");
+        }
+
+    }, [selectedCountry, selectedState])
+
+
+    return (<div 
+    style={{
+        display : "flex",
+        flexDirection : "column",
+        justifyContent : "center",
+        alignItems : "center"
+    }}
+    >
         <h1>Select Location</h1>
-        <select name="Countries"> 
-            <option value="">Select Countries</option>
+
+        <div>
+        <select 
+        onChange={(e) => {
+            setSelectedCountry(e.target.value);
+        }}
+        name="Countries"> 
+            <option value="">Select Country</option>
+            {
+                countryData.map((country) => <option key={country} value={country} >{country}</option> )
+            }
         </select>
+
+        <select 
+        onChange={(e) => {
+            setSeletedState(e.target.value);
+        }}
+        name="States" disabled={selectedCountry==="" ? true : null} > 
+            <option value="">Select State</option>
+            {
+                stateData.map((state) => <option key={state} value={state} >{state}</option>)
+            }
+        </select>
+
+        <select 
+        onChange={(e) => {
+            setSelectedCity(e.target.value);
+        }}
+        name="Cities" disabled={(selectedState==="") ? true : null} > 
+            <option value="">Select City</option>
+            {
+                cityData.map((city) => <option key={city} value={city} >{city}</option>)
+            }
+        </select>
+        </div>
+
+        {/* Conditional render */}
+        {
+            selectedCity && <h3>You selected <span style={{fontSize : "2rem"}}>{selectedCountry}</span>, <span style={{fontSize : "1.5rem", color : "grey"}} >{selectedState}</span>, <span style={{fontSize : "1rem", color : "grey"}} >{selectedCity}</span></h3>
+        }
+
+        
+
     </div>)
 }
 
